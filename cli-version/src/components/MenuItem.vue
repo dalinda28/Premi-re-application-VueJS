c59fe3f5a485216be5c16898562a4a46dd9ade1d
dalinda28 <aloui.dalinda28@gmail.com>
2020-06-14 12:54:39 +0200

    codage

---
+++ cli-version/src/components/MenuItem.vue
@@ -0,0 +1,47 @@
+<script>
+export default {
+	name: "MenuItem",
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
+}
+</script>
+
+<template>
+	<div class="menu-item">
+		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
+		<div>
+			<h3>{{ name }}</h3>
+			<p>Prix : {{ generatedPrice }}</p>
+			<p v-if="inStock">En stock</p>
+			<p v-else>En rupture de stock</p>
+			<div>
+				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
+				<input v-model.number="quantity" id="add-item-quantity" type="number" />
+				<button @click="addToShoppingCart(quantity)">
+					Ajouter au panier
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style></style>
